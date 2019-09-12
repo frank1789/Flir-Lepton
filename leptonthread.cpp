2fffa93ed5f69ea5661eeb070f1d413c7ca9eaee
Francesco <francesco.argentieri89@gmail.com>
2019-09-12 16:32:43 +0200

    add: intermediate buffer

--- leptonthread.cpp
+++ leptonthread.cpp
@@ -1,14 +1,26 @@
 #include "leptonthread.hpp"
+#include "logger.h"
 
 LeptonThread::LeptonThread() : QThread(), m_image(m_camera) {
+    m_frameU8 = new QVector<uint8_t>(m_camera.width() * m_camera.height());
 }
 
 LeptonThread::~LeptonThread() {
     m_camera.stop();
+    delete m_frameU8;
+#if LOGGER
+    LOG(INFO, "dtor LeptonThread")
+#endif
 }
 
-void LeptonThread::run()
-{
-    m_camera.start();
+void LeptonThread::run() {
+  m_camera.start();
+  auto temperature = m_camera.SensorTemperature();
+#if LOGGER
+  LOG(DEBUG, "temperature acquired: %lf", temperature)
+#endif
+  while (true) {
+       m_camera.getFrameU8(*m_frameU8);
+       m_image.setImage(*m_frameU8);
+  }
 }
-
--- leptonthread.hpp
+++ leptonthread.hpp
@@ -1,6 +1,8 @@
 #ifndef LEPTONTHREAD_HPP
 #define LEPTONTHREAD_HPP
 
+#include <cstdint>
+#include <QVector>
 #include <QObject>
 #include <QThread>
 #include "leptoncamera.h"
@@ -21,6 +23,7 @@ signals:
 private:
     LeptonCamera m_camera;
     ThermalImage m_image;
+    QVector<uint8_t> *m_frameU8;
 };
 
 #endif // LEPTONTHREAD_HPP
