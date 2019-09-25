b4126b040ac5acddab1064374b35118b46765a53
Francesco <francesco.argentieri89@gmail.com>
2019-09-25 16:43:29 +0200

    fix: timing parameterized

--- leptonthread.hpp
+++ leptonthread.hpp
@@ -14,6 +14,14 @@ constexpr int PACKET_SIZE_UINT16{PACKET_SIZE / 2};
 constexpr int PACKETS_PER_FRAME{60};
 constexpr int FRAME_SIZE_UINT16{PACKET_SIZE_UINT16 * PACKETS_PER_FRAME};
 
+// Lepton communication timing parameters
+constexpr uint16_t MaxResetsPerSegment{750};   // packet resets
+constexpr uint16_t kMaxResetsPerFrame{30};     // segment resets
+constexpr uint16_t kMaxResetsBeforeReboot{2};  // frame resets
+constexpr uint32_t LeptonLoadTime{2000};       // 0.002 s = 2 ms = 2000 us
+constexpr uint32_t LeptonResetTime{1000};      // 0.001 s = 1 ms = 3000 us
+constexpr uint32_t LeptonRebootTime{750000};   // 0.75 s = 750 ms = 750000 us
+
 /**
  * @brief Class control Flir Lepton Camera 2.5
  *
@@ -45,7 +53,7 @@ class LeptonThread : public QThread {
   const int* colorMap;  // associate colors to the frame
  public slots:
   void performFFC();
-  void changeColourMap(const int *colour);
+  void changeColourMap(const int* colour);
   void snapImage();
 
  signals:
