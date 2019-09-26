e9ec3f8fa66f56e2335f291f9f048523f31cc34c
Francesco <francesco.argentieri89@gmail.com>
2019-09-26 08:18:02 +0200

    fix: name file

---
+++ mylabel.cpp
@@ -0,0 +1,15 @@
+#include "mylabel.hpp"
+#include <QColor>
+#include <QPainter>
+#include "log/logger.h"
+
+MyLabel::MyLabel(QWidget *parent) : QLabel(parent) {}
+
+MyLabel::~MyLabel() {}
+
+// when the system calls setImage, we'll set the label's pixmap
+void MyLabel::setImage(QImage image) {
+  QPixmap pixmap = QPixmap::fromImage(image);
+  setPixmap(pixmap.scaled(this->width(), this->height(), Qt::KeepAspectRatio));
+}
+
