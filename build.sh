e9cfc2c9629620265c5136545885e9d5fc113dc4
Francesco <francesco.argentieri89@gmail.com>
2020-04-17 12:56:49 +0200

    add: update script

--- build.sh
+++ build.sh
@@ -1,44 +1,7 @@
 #!/usr/bin/env sh
 
-# This script provides two functions:
-#   - "compile_debug": check if there are folders containing previous builds.
-#                    Compile the project in debug mode, then once completed,
-#                    start the unit test.
-#                    Collects information for code coverage.
-#
-#   - "compile_release": check if there are folders containing previous builds.
-#                       Fill in the project and start the executable.
-#
-# Args:
-#    - $1 the first argument is optional and is used to activate the debug mode.
-#      Is required to specify "debug" or "debug" does not accept other arguments.
-#
-# Usage:
-#
-# compile in debug mode:
-# $./build.sh debug
-#
-# # compile in release mode:
-# $./build.sh
-#
-
-# ////////////////////////////////////////////////////////////////////////////
-# declare function
-# ////////////////////////////////////////////////////////////////////////////
-
-compile_debug() {
-    DIRECTORY=build_debug
-    clear_old_build
-    echo "create new directory $DIRECTORY"
-    mkdir $DIRECTORY && cd $DIRECTORY
-    cmake -D CMAKE_BUILD_TYPE=Debug ..
-    make
-    EXECUTABLE=$(find $PWD -name "FlirLepton")
-    ${EXECUTABLE}
-}
-
 compile_release() {
-    DIRECTORY=build_release
+    DIRECTORY=build
     clear_old_build
     echo "create new directory $DIRECTORY"
     mkdir $DIRECTORY && cd $DIRECTORY
@@ -71,13 +34,5 @@ clear_old_build() {
 # ////////////////////////////////////////////////////////////////////////////
 # main script
 # ////////////////////////////////////////////////////////////////////////////
+compile_release
 
-if [ "$#" -eq "0" ]; then
-    echo "**** build release mode ****"
-    compile_release
-else
-    if [ "$1" = "Debug" ] || [ "$1" = "debug" ]; then
-        echo "**** build debug mode ****"
-        compile_debug
-    fi
-fi
