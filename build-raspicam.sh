b69e301329bef8d88b9e14e0d67515ee96f0278a
Francesco <francesco.argentieri89@gmail.com>
2020-04-24 14:12:00 +0200

    add: script in separate file
    del: remove unused

---
+++ build-raspicam.sh
@@ -0,0 +1,14 @@
+!#usr/bin/env sh
+
+# setup folder
+raspicam=$(find $PWD -type d -name "raspicam-*")
+echo ${raspicam} 
+cd ${raspicam} 
+mkdir build-raspi 
+cd build-raspi && echo $PWD 
+
+# configure cmake 
+cmake ..
+
+# compile and install
+make -j$(nproc) && make install && ldconfig && cd .. && \
